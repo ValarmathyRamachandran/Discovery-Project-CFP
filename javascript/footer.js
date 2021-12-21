4f3c3fe228135ddbfae2f05ef129823952cb5b91
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-12-21 18:11:51 +0530

    Created separate files forf header and footer js and linked it to ourWorld page

---
+++ javascript/footer.js
@@ -0,0 +1,64 @@
+document.getElementById("footer").innerHTML= `
+    <div class="footer-container"></div>
+    <nav class="c-site-foot__nav">
+        <div class="c-site-foot__menu">
+          <h4 class="c-site-foot__title">Get in touch</h4>
+
+          <ul class="c-site-foot-menu">
+            <li class="c-site-foot-menu__item">
+              <a href="tel:(480) 624-5200" class="c-site-foot-menu__link | js-site-link">(480) 624-5200</a>
+            </li>
+            <li class="c-site-foot-menu__item">
+              <a href="mailto:mail@discoverylandco.com" style="text-decoration: underline !important;"class="c-site-foot-menu__link c-site-foot-menu__link--email | js-site-link">mail@discoverylandco.com</a>
+            </li>
+          </ul>
+        </div>
+        <div class="c-site-foot__item c-site-foot__menus">
+          <ul class="c-site-foot-menu --main">
+
+                <li class="c-site-foot-menu__item">
+                <a href="/about-dlc" class="c-site-foot-menu__link | js-site-link">About DLC</a>
+              </li>
+                          <li class="c-site-foot-menu__item">
+                <a href="/our-worlds" class="c-site-foot-menu__link | js-site-link">Our Worlds</a>
+              </li>
+                          <li class="c-site-foot-menu__item">
+                <a href="/experiences" class="c-site-foot-menu__link | js-site-link">Experiences</a>
+              </li>
+                          <li class="c-site-foot-menu__item">
+                <a href="/gallery" class="c-site-foot-menu__link | js-site-link">Gallery</a>
+              </li>
+                          <li class="c-site-foot-menu__item">
+                <a href="/press" class="c-site-foot-menu__link | js-site-link">Press</a>
+              </li>
+            
+          </ul>
+          <ul class="c-site-foot-menu --sub">
+
+                          <li class="c-site-foot-menu__item">
+                <a href="/discovery-life-magazine" class="c-site-foot-menu__link | js-site-link">Magazine</a>
+              </li>
+                          <li class="c-site-foot-menu__item">
+                <a href="/careers" class="c-site-foot-menu__link | js-site-link">Careers</a>
+              </li>
+                          <li class="c-site-foot-menu__item">
+                <a href="/foundation" class="c-site-foot-menu__link | js-site-link">Foundation</a>
+              </li>
+                          <li class="c-site-foot-menu__item">
+                <a href="/company" class="c-site-foot-menu__link | js-site-link">Company</a>
+              </li>
+                          <li class="c-site-foot-menu__item">
+                <a href="/contact" class="c-site-foot-menu__link | js-site-link">Contact</a>
+              </li>
+                          <li class="c-site-foot-menu__item">
+                <a href="/privacy-policy" class="c-site-foot-menu__link | js-site-link">Privacy</a>
+              </li>
+            
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <div class="c-site-foot__bottom">
+      <p>© Discovery Land Company. All rights reserved.</p>
+    </div>
+</div>`
---
+++ javascript/header.js
@@ -0,0 +1,22 @@
+document.getElementById("header").innerHTML= `
+<div class="header-container">
+<div id ="logo"><img src="/assets/Images/logo.png" alt="logo" width="32" height="32">
+<div class ="header-content">Discovery</div>
+<div id="logo-content"><sub>Land Company</sub>
+</div></div>
+
+<nav class="header-nav">
+    <div id="header-nav-menu">
+        <a class="menu-item" href="#">About DLC</a>
+        <a class="menu-item" href="#">Our Worlds</a>
+        <a class="menu-item" href="#">Experiences</a>
+        <a class="menu-item" href="#">Gallery</a>
+        <a class="menu-item" href="#">Press</a>
+        <a><div class="ham" >  
+            <span class="bar1"></span>  
+            <span class="bar2"></span>  
+            <span class="bar3"></span>  
+        </div> </a>
+    </div>
+</nav>
+</div>`
